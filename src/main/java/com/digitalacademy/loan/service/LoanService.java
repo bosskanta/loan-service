799035e8b254f5f11a850179a21c10772b863365
KANTAPON SORNKERD <s92868@scbcorp.co.th>
2021-06-25 13:45:25 +0700

    first commit

---
+++ src/main/java/com/digitalacademy/loan/service/LoanService.java
@@ -0,0 +1,38 @@
+package com.digitalacademy.loan.service;
+
+import com.digitalacademy.loan.constants.LoanError;
+import com.digitalacademy.loan.exception.LoanException;
+import com.digitalacademy.loan.model.LoanInfo;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoanService {
+    private static final Logger log = LogManager.getLogger(LoanService.class.getName());
+
+    public LoanInfo getLoanInfoById(Long id) throws Exception {
+        log.info("Get loan info by id: {}", id);
+        LoanInfo loanInfo = new LoanInfo();
+
+        if (id.equals(1L)) {
+            loanInfo.setId(1L);
+            loanInfo.setStatus("OK");
+            loanInfo.setAccountPayable("102-222-2200");
+            loanInfo.setAccountReceivable("102-333-2020");
+            loanInfo.setPrincipalAmount(3400000.00);
+        }
+        else if (id.equals(2L)) {
+            log.info("Id: {}", id);
+            throw new LoanException(LoanError.GET_LOAN_INFO_NOT_FOUND,
+                    HttpStatus.BAD_REQUEST);
+        }
+        else {
+            log.info("Id: {}", id);
+            throw new Exception("Test throw new exception");
+        }
+
+        return loanInfo;
+    }
+}
